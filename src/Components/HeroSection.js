76bf03b69e07ed5932b1fdf072835dc55bbdcf86
Avantika11201 <avantikajewani.bscsf21@iba-suk.edu.pk>
2023-12-20 16:40:02 +0500

    commit

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -13,9 +13,9 @@ export const HeroSection = () => {
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col">
-                            <button class="btn-demo">
-                                {/* <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PlayCircleFilledWhiteIcon" style={{color: "white"}}><path transform="scale(0.5, 0.5)" d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path></svg> */}
+                    <div className="col d-flex justify-content-center mt-5">
+                            <button class="btn-demo ">
+                                <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PlayCircleFilledWhiteIcon" style={{color: "white"}}><path transform="scale(0.5, 0.5)" d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path></svg>
                             </button>
                         </div>
 
